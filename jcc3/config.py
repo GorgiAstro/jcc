7fc5aeb4ed2631d39f2e2927a6b4c49458801838
Clément Jonglez <clement@jonglez.space>
2023-11-26 18:27:08 +0100

    add jcc from https://github.com/GorgiAstro/pylucene

---
+++ jcc3/config.py
@@ -0,0 +1,8 @@
+
+INCLUDES=['/home/yzokras/.jdk/jdk-8/include', '/home/yzokras/.jdk/jdk-8/include/linux']
+CFLAGS=['-fno-strict-aliasing', '-Wno-write-strings']
+DEBUG_CFLAGS=['-O0', '-g', '-DDEBUG']
+LFLAGS=['-L/home/yzokras/.jdk/jdk-8/jre/lib/amd64', '-ljava', '-L/home/yzokras/.jdk/jdk-8/jre/lib/amd64/server', '-ljvm', '-Wl,-rpath=/home/yzokras/.jdk/jdk-8/jre/lib/amd64:/home/yzokras/.jdk/jdk-8/jre/lib/amd64/server']
+IMPLIB_LFLAGS=[]
+SHARED=True
+VERSION="3.13"
