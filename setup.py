218bbbd7f53ee83bdf2a2ea0a86183b4e41637de
Clément Jonglez <clement@jonglez.space>
2023-12-12 12:15:36 +0100

    fix path to JDK installed by install-jdk

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ JDK = {
 if 'JCC_JDK' in os.environ:
     JDK[platform] = os.environ['JCC_JDK']
 
-JDK_VER = '8'
+JDK_VER = '11' # Don't use JDK8, it has a different directory structure......
 JDK4PY_HOME = None
 try:
     JDK4PY_HOME = jdk.install(JDK_VER)
@@ -89,9 +89,13 @@ except jdk.JdkError:
     home_folder = os.path.expanduser('~')
     jdk_root_folder = os.path.join(home_folder, ".jdk")
     for file in os.listdir(jdk_root_folder):
-        if os.path.isdir(os.path.join(jdk_root_folder, file)) and file.startswith(f"jdk{JDK_VER}"):
-            JDK4PY_HOME = os.path.join(jdk_root_folder, file)
-            break
+        if os.path.isdir(os.path.join(jdk_root_folder, file)) and file.startswith("jdk"):
+            file_stripped = file.strip("jdk")
+            if file_stripped.startswith("-"):
+                file_stripped = file_stripped.strip("-")
+            if file_stripped.startswith(JDK_VER):
+                JDK4PY_HOME = os.path.join(jdk_root_folder, file)
+                break
 if JDK4PY_HOME is not None:
     JDK[platform] = JDK4PY_HOME
 
