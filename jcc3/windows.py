9960a559a225990b8b26de57a9fdc17cba624422
Clément Jonglez <clement@jonglez.space>
2023-12-13 15:04:28 +0100

    windows: use JCC_JDK defined in config.py before trying to find it in env variables

--- jcc3/windows.py
+++ jcc3/windows.py
@@ -74,8 +74,11 @@ def get_jvm_dll_directory_from_registry(client_or_server):
 
 
 def get_jvm_dll_directory_from_env(client_or_server):
-
-    path = os.getenv('JCC_JDK') or os.getenv('JAVA_HOME')
+    try:
+        from jcc.config import JCC_JDK
+        path = JCC_JDK
+    except:
+        path = os.getenv('JCC_JDK') or os.getenv('JAVA_HOME')
     if path:
         # Traverse the found path to identify if there is a jvm.dll somewhere
         for location in (('bin', client_or_server),
